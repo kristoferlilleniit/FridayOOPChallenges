9f8314cf640896400285dbbec4e774e02410394b
Opilane TTHK <opilane@tthk.ee>
2020-11-27 09:51:58 +0200

    first commit

---
+++ Cat/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Cat
+{
+    class Program
+    {
+
+        class Cat
+        {
+            string name;
+            string color;
+            double hunger;
+
+            public Cat(string _name, string _color)
+            {
+                name = _name;
+                color = _color;
+                hunger = 0;
+            }
+
+            public string Name
+            {
+                get { return name; }
+                set
+                {
+                    Name = value;
+                }
+            }
+
+            public string Color
+            {
+                get { return color; }
+            }
+
+            public double Hunger
+            {
+                get { return hunger; }
+            }
+
+            public void Sleep()
+            {
+                Console.WriteLine("The cat sleeps.");
+                hunger += 0.2;
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Cat newCat = new Cat("Kass", "Black");
+
+            while(newCat.Hunger != 1)
+            {
+                newCat.Sleep();
+            }
+
+            Console.WriteLine("Meow");
+        }
+    }
+}
---
+++ Pet/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace Pet
+{
+    class Program
+    {
+
+        class Pet
+        {
+            string name;
+            int age;
+            double weight;
+            int lifespan;
+
+            public Pet(string _name, double _weight)
+            {
+                name = _name;
+                age = 0;
+                weight = _weight;
+                lifespan = 0;
+            }
+
+            public string Name
+            {
+                get { return Name; }
+            }
+
+            public int Age
+            {
+                get { return Age; }
+            }
+
+            public double Weight
+            {
+                get { return weight; }
+            }
+
+            public int Lifespan
+            {
+                get { return lifespan; }
+            }
+
+            public void PetStats()
+            {
+                Console.WriteLine($"Name: {name} \n Age: {age}");
+            }
+
+            public void PetLifeSpan()
+            {
+                Console.WriteLine($"Lifespan: {lifespan}");
+            }
+
+            public void PetWeight()
+            {
+                Console.WriteLine($"Weight: {weight}");
+            }
+
+            public void Eat()
+            {
+                weight += 0.1;
+                Console.WriteLine($"{name} eats.");
+            }
+
+            public void Exercise()
+            {
+                weight -= 0.1;
+                Console.WriteLine($"{name} exercises.");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Pet Hamster = new Pet("Hamster", 0);
+
+            for (int i = 0; i < 11; i++)
+            {
+                if (Hamster.Weight == 0.7)
+                {
+                    Hamster.Exercise();
+                }else
+                {
+                    Hamster.Eat();
+                }
+
+            }
+
+            Console.WriteLine("The hamster died:(");
+
+        }
+    } 
+}
